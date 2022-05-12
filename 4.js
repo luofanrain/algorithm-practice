e1a712b7784a1a7a32151b0641534f116b9251ec
lixiandong <1779333834@qq.com>
2022-05-12 17:31:30 +0800

    贪心算法

--- 4.js
+++ 4.js
@@ -1,4 +1,4 @@
-// 贪心算法
+// 贪心算法 每次选当前最优解，直到目的达成
 function mergeALL(left,right){
   return  new Set([...left, ...right])
 }
@@ -13,7 +13,7 @@ function mergeNotHave(left,right){
   return mergeALL(currentLeft,currentRight)
 }
 
-let list = new Set([2,5,7])
+let list = new Set([1,2,3,4,5,6,7])
 
 let library = [
   [1,2,3],
@@ -22,14 +22,26 @@ let library = [
   [4,7,6],
   [1,5,7]
 ]
+let allInfos = new Map()
+library.forEach((value,index)=>{
+  allInfos.set(index,new Set(value))
+})
 let result = []
 function dealWith(){
-  for(let index=0;index<library.length;index++){
-    let value = library[index]
-    let items = mergeALL(list,value)
-    if(items.length )
-  }
+  while(list.size>0){
+    let coverd = new Set()
+    let bestGroup = new Set()
+    allInfos.forEach(value=>{
+      let items = mergeHave(list,value)
+      if(items.size > coverd.size){
+        bestGroup = value;
+        coverd = items
+      }
 
+    })
+    list = mergeNotHave(list,coverd)
+    result.push(bestGroup)
+  }
 }
 let time = Date.now()
 dealWith()
